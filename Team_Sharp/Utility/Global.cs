95b8c366bcf73a6c834ca408581eaa44a7e128ae
unknown <Nahiyan3>
2023-05-05 21:12:43 +0600

    All utility classes added

--- Team_Sharp/Utility/Global.cs
+++ Team_Sharp/Utility/Global.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Team_Sharp.Utility
 {
-    internal class Global
+    public static class Global
     {
+        public static string _userName { get; set; }
+        public static string _language { get; set; }
+        public static string _gender { get; set; }
+        public static string _name { get; set; }
+        public static string _email { get; set; }
+        public static string _DOB { get; set; }
+
     }
 }
